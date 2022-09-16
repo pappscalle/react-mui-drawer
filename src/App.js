e4c21a7740c9a24ae3c87068c4c498c8844f1d21
Carl Frenn <carl.frenn@gmail.com>
2022-09-16 16:49:23 +0200

    updated Drawer

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
 import React, { useState } from 'react'
 import './App.css'
 import {
-    Button,
-    Drawer,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemText,
-    Typography,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
 } from '@mui/material'
 import { Box } from '@mui/system'
 
+const items = ['First item', 'Second item', 'Loooooooong item']
+
 const App = () => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    const toggleDrawer = () => {
-        setOpen(!open)
-    }
+  const toggleDrawer = () => {
+    setOpen(!open)
+  }
 
-    return (
-        <>
-            <Typography>Hello world!!</Typography>
-            <Button onClick={() => toggleDrawer()}>Click me!</Button>
-            <Drawer anchor="left" open={open}>
-                <Box onClick={() => toggleDrawer()}>
-                    <List>
-                        {['Inbox', 'Send email', 'Drafts'].map(
-                            (text, index) => (
-                                <ListItem key={text}>
-                                    <ListItemButton
-                                        onClick={() => toggleDrawer()}
-                                    >
-                                        <ListItemText primary={text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            )
-                        )}
-                    </List>
-                </Box>
-            </Drawer>
-        </>
-    )
+  return (
+    <>
+      <Typography paragraph>Hello world!!</Typography>
+      <Button onClick={() => toggleDrawer()}>Click me!</Button>
+      <Drawer anchor="left" open={open}>
+        <Box>
+          <List>
+            {items.map((text, index) => (
+              <ListItem key={text}>
+                <ListItemButton onClick={() => toggleDrawer()}>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </>
+  )
 }
 
 export default App
